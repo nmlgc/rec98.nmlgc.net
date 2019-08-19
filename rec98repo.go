f280ffdcc5ce90e8c690bd526adfd7dc045adff4
nmlgc <nmlgc@nmlgc.net>
2019-08-19 19:16:24 +0200

    List all non-reverse-engineered game source files we've ever had
    Part of P0039, funded by Splashman.

---
+++ rec98repo.go
@@ -0,0 +1,45 @@
+package main
+
+type gameSource struct {
+	Init      []string
+	OP        []string
+	Main      []string
+	Cutscenes []string
+}
+
+func (gs gameSource) All() []string {
+	var ret []string
+	ret = append(ret, gs.Init...)
+	ret = append(ret, gs.OP...)
+	ret = append(ret, gs.Main...)
+	return append(ret, gs.Cutscenes...)
+}
+
+var gameSources = []gameSource{
+	{
+		[]string{"th01_zunsoft.asm"},
+		[]string{"th01_op.asm"},
+		[]string{"th01_reiiden.asm", "th01_reiiden_2.inc"},
+		[]string{"th01_fuuin.asm"},
+	}, {
+		[]string{"th02_zuninit.asm", "th02_zun_res.asm"},
+		[]string{"th02_op.asm"},
+		[]string{"th02_main.asm"},
+		[]string{"th02_maine.asm"},
+	}, {
+		[]string{"th03_res_yume.asm", "th03_zunsp.asm"},
+		[]string{"th03_op.asm"},
+		[]string{"th03_main.asm"},
+		[]string{"th03_mainl.asm"},
+	}, {
+		[]string{"th04_res_huma.asm"},
+		[]string{"th04_op.asm"},
+		[]string{"th04_main.asm", "th04_main_seg3+4.inc"},
+		[]string{"th04_maine.asm"},
+	}, {
+		[]string{"th05_res_kso.asm"},
+		[]string{"th05_op.asm"},
+		[]string{"th05_main.asm", "th05_main_seg3+4.inc"},
+		[]string{"th05_maine.asm"},
+	},
+}

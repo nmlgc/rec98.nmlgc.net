1ffc8d86614871a8b491078990cc2b10abc09e66
nmlgc <nmlgc@nmlgc.net>
2019-12-30 02:04:49 +0100

    [🚚] Deliver P0064 (Completing TH04/TH05 OP.EXE position independence)
    Summary: https://rec98.nmlgc.net/blog/2019-12-29
    
    Demo video: https://www.youtube.com/watch?v=iqPxiaaHIhA
    
    Funded by Touhou Patch Center.

--- static/main.js
+++ static/main.js
@@ -11,3 +11,11 @@ function formatCurrency(cents) {
 	document.write(numFmt.format(cents / 100))
 }
 // --------
+
+// Enabling external content
+// -------------------------
+function enableExternal(id_consent, id_target, callback) {
+	callback(document.getElementById(id_target));
+	document.getElementById(id_consent).hidden = true;
+}
+// -------------------------

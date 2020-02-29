855aef12a2fbf3f1e0fadc5e482eed4cb99b68ee
nmlgc <nmlgc@nmlgc.net>
2020-02-29 18:11:36 +0100

    [Blog] Enable all external sources from one hostname with a click on each link

--- static/main.js
+++ static/main.js
@@ -14,8 +14,46 @@ function formatCurrency(cents) {
 
 // Enabling external content
 // -------------------------
-function enableExternal(id_consent, id_target, callback) {
-	callback(document.getElementById(id_target));
-	document.getElementById(id_consent).hidden = true;
+var externals = {};
+var consent_ids = {};
+
+function externalRegister(day, target, url) {
+	const hostname = new URL(url).hostname;
+	const target_id = `${day}-${target}`;
+	const consent_id = `${day}-consent`;
+	if(externals[hostname] === undefined) {
+		externals[hostname] = {}
+	}
+	if(consent_ids[hostname] === undefined) {
+		consent_ids[hostname] = []
+	}
+	if(document.getElementById(consent_id) === null) {
+		document.write(`
+			<a
+				href="#${consent_id}"
+				id="${consent_id}"
+				onClick="externalEnable('${hostname}'); return false;"
+			>(click to enable external content from <code>${hostname}</code>)
+			</a>`
+		);
+		consent_ids[hostname].push(consent_id);
+	}
+	if(externals[hostname][target_id] !== undefined) {
+		alert(
+			'Target ID "#' + target_id + '" already registered as \"'
+			+ externals[hostname][target_id] + '"'
+		)
+		return;
+	}
+	externals[hostname][target_id] = url;
+}
+
+function externalEnable(hostname) {
+	for(let target_id in externals[hostname]) {
+		document.getElementById(target_id).src = externals[hostname][target_id];
+	}
+	for(let consent_id of consent_ids[hostname]) {
+		document.getElementById(consent_id).hidden = true;
+	}
 }
 // -------------------------

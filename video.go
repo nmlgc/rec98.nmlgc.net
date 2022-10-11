6594742847261363b2e808e275c5bc086b7d0f83
nmlgc <nmlgc@nmlgc.net>
2022-10-12 01:00:29 +0200

    [Videos] [Pipeline] Define per-codec file extensions
    It might seem as if we're only ever going to ship .webm, but we do also
    need to handle .avi (for the ZMBV source files) and .webp (for
    posters).
    
    Part of P0218, funded by [Anonymous] and Yanga.

--- video.go
+++ video.go
@@ -8,13 +8,18 @@ import "path"
 // VideoDir defines a directory that all videos should be placed into.
 type VideoDir struct {
 	Dir string // under VideoRoot.Root.LocalPath
+	FFMPEGCodec
 }
 
 // Best web-supported lossless codec in 2019
-var VP9 = VideoDir{"vp9"}
+var VP9 = VideoDir{"vp9", FFMPEGCodec{
+	Ext: ".webm",
+}}
 
 // Lossy fallback for outdated garbage
-var VP8 = VideoDir{"vp8"}
+var VP8 = VideoDir{"vp8", FFMPEGCodec{
+	Ext: ".webm",
+}}
 
 // VIDEO_ENCODERS defines all target codecs, ordered from the most to the least
 // preferred one.
@@ -31,5 +36,15 @@ func NewVideoRoot(root SymmetricPath) *VideoRoot {
 }
 
 func (r *VideoRoot) URL(stem string, vd *VideoDir) string {
-	return path.Join(r.Root.URLPrefix, vd.Dir, (stem + ".webm"))
+	return path.Join(r.Root.URLPrefix, vd.Dir, (stem + vd.Ext))
 }
+
+// ffmpeg
+// ------
+
+// FFMPEGCodec defines ffmpeg parameters for a codec.
+type FFMPEGCodec struct {
+	Ext string // File extension
+}
+
+// ------

152ab0d3125f056927f8f92cfc134a3f5db54a80
nmlgc <nmlgc@nmlgc.net>
2023-04-10 15:55:49 +0200

    [Order] Move non-PayPal-related order code to `order.ts`
    Part of P0238, funded by Ember2528.

--- static/order.ts
+++ static/order.ts
@@ -1,5 +1,11 @@
 "use strict";
 
+let mailto_support = "support@nmlgc.net";
+
+const form = document.querySelector("form")!;
+
+const cust_name = document.getElementById("cust_name") as HTMLSelectElement;
+const cust_url = document.getElementById("cust_url") as HTMLSelectElement;
 const metric = document.getElementById("metric") as HTMLSelectElement;
 const goal = document.getElementById("goal") as HTMLInputElement;
 const onetime = document.getElementById("onetime") as HTMLInputElement;
@@ -10,6 +16,44 @@ const info_text = document.getElementById("info_text")!;
 const micro_container = document.getElementById("micro_container")!;
 const micro = document.getElementById("micro") as HTMLInputElement;
 const micro_available = document.getElementById("micro_available")!;
+
+const amount = document.getElementById("amount") as HTMLInputElement;
+const discount = document.getElementById("discount") as HTMLSelectElement;
+const discount_breakdown = document.getElementById("discount_breakdown")!;
+const discount_sponsor = document.getElementById("discount_sponsor")!;
+const roundup_amount = document.getElementById("roundup_amount")!;
+const roundup_pushes = document.getElementById("roundup_pushes")!;
+const roundup_noun = document.getElementById("roundup_noun")!;
+
+const error = document.getElementById("error")!;
+
+function HTMLSupportMail() {
+	return `
+<a href="mailto:` + mailto_support + `"><kbd>` + mailto_support + `</kbd></a>`;
+}
+
+/**
+ * Must match the implementation in db_discount_offers.go!
+ *
+ * @param capRemainingBeforeAmount In €.
+ * @param amount In €.
+ * @param pushprice In €.
+ * @param discountFraction Fraction of a push covered by the sponsor.
+ * @returns Round-up € funded by the sponsor, limited to the cap.
+ */
+function discountRoundupValue(
+	capRemainingBeforeAmount: number,
+	amount: number,
+	pushprice: number,
+	discountFraction: number
+) {
+	const pushprice_discounted = (pushprice * (1 - discountFraction));
+	const roundup_value = (pushprice - pushprice_discounted);
+	return Math.min(
+		((amount / pushprice_discounted) * roundup_value),
+		(capRemainingBeforeAmount - amount)
+	);
+}
 
 let micro_previously_checked = false;
 function handleSelect(option: HTMLOptionElement) {
@@ -42,6 +86,145 @@ function handleSelect(option: HTMLOptionElement) {
 	micro_container.hidden = false;
 }
 
+function clampNumber(obj) {
+	obj.value = Math.max(obj.value, obj.min);
+	obj.value = Math.min(obj.value, obj.max);
+	return obj
+}
+
+function formatNumber(obj, digits) {
+	return parseFloat(obj.value).toFixed(digits);
+}
+
+function isOneTime() {
+	return onetime.checked;
+}
+
+function onCycle() {
+	const push_amount = document.getElementById("push_amount")!;
+	const push_noun = document.getElementById("push_noun")!;
+
+	const pushprice = (Number(push_amount.dataset.price) / 100);
+
+	const updatePushAmount = function(
+		target_amount: HTMLElement, target_noun: HTMLElement, money: number
+	) {
+		target_amount.innerHTML = (
+			(Math.round((money / pushprice) * 100) / 100).toString()
+		);
+		target_noun.innerHTML = ((money == pushprice) ? " push" : " pushes");
+	}
+
+	const onetime = isOneTime();
+	if(onetime) {
+		amount.onchange = function() {
+			const value_from_customer = clampNumber(amount);
+			updatePushAmount(push_amount, push_noun, Number(amount.value));
+			amount.value = formatNumber(value_from_customer, 2);
+
+			if(!discount) {
+				return;
+			}
+
+			const discount_offer = discount.options[discount.selectedIndex];
+			const discount_fraction = Number(discount_offer.dataset.fraction);
+			if(discount_offer.index === 0) {
+				discount_breakdown.hidden = true;
+			} else {
+				discount_breakdown.hidden = false;
+				discount_sponsor.innerHTML = discount_offer.dataset.name!;
+				const roundup_value = discountRoundupValue(
+					Number(amount.max),
+					Number(amount.value),
+					pushprice,
+					discount_fraction
+				)
+				updatePushAmount(roundup_pushes, roundup_noun, roundup_value);
+				roundup_amount.innerHTML = valueInCurrency(roundup_value * 100);
+			}
+		}
+		amount.min = "1.00";
+		amount.step = "0.01";
+
+		if(discount) {
+			discount.disabled = false;
+			discount_breakdown.hidden = false;
+		}
+	} else {
+		amount.onchange = function() {
+			amount.value = formatNumber(amount, 0);
+			updatePushAmount(push_amount, push_noun, Number(amount.value));
+		}
+		amount.min = "1";
+		amount.step = "1";
+
+		if(discount) {
+			discount.disabled = true;
+			discount.selectedIndex = 0;
+			discount_breakdown.hidden = true;
+		}
+	}
+	amount.onchange();
+	paypalOnCycle(onetime);
+}
+
+function validateForm() {
+	for (const el of form.querySelectorAll("input[required]")) {
+		if (!(el as HTMLInputElement).reportValidity()) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function startTransaction() {
+	error.hidden = true;
+	document.querySelector("html")!.classList.add("wait");
+}
+
+function endTransaction() {
+	document.querySelector("html")!.classList.remove("wait");
+}
+
+function cycle() {
+	return isOneTime() ? "onetime" : "monthly";
+}
+
+function thankyou() {
+	return form.submit();
+}
+
+async function sendIncoming(provider_session: string) {
+	let response = await fetch('/api/transaction-incoming', {
+		method: 'post',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify({
+			ProviderSession: provider_session,
+			CustName: cust_name.value,
+			CustURL: cust_url.value,
+			Metric: metric.value,
+			Goal: goal.value,
+			Micro: micro.checked,
+			Cycle: cycle(),
+			Discount: ((isOneTime() && discount) ? discount.selectedIndex : 0),
+			Cents: (Number(amount.value)) * 100,
+		})
+	});
+	if(!response.ok) {
+		error.innerHTML =
+			"Something went wrong: " + await response.text() + "<br>" +
+			"I should have received your order though, and will confirm it " +
+			"as soon as I see it.";
+		error.hidden = false;
+		endTransaction();
+		return false;
+	}
+	return true;
+}
+
 window.onload = () => {
 	handleSelect(metric.options[metric.selectedIndex]);
+	onCycle();
 }
--- static/paypal.ts
+++ static/paypal.ts
@@ -1,113 +1,14 @@
 "use strict";
 
-let mailto_support = "support@nmlgc.net";
-
-const cust_name = document.getElementById("cust_name") as HTMLSelectElement;
-const cust_url = document.getElementById("cust_url") as HTMLSelectElement;
-
-const form = document.querySelector("form")!;
-const amount = document.getElementById("amount") as HTMLInputElement;
-const discount = document.getElementById("discount") as HTMLSelectElement;
-const discount_breakdown = document.getElementById("discount_breakdown")!;
-const discount_sponsor = document.getElementById("discount_sponsor")!;
-const roundup_amount = document.getElementById("roundup_amount")!;
-const roundup_pushes = document.getElementById(	"roundup_pushes")!;
-const roundup_noun = document.getElementById("roundup_noun")!;
-
-const error = document.getElementById("error")!;
-
-function HTMLSupportMail() {
-	return `
-<a href="mailto:` + mailto_support + `"><kbd>` + mailto_support + `</kbd></a>`;
-}
-
-/**
- * Must match the implementation in db_discount_offers.go!
- *
- * @param capRemainingBeforeAmount In €.
- * @param amount In €.
- * @param pushprice In €.
- * @param discountFraction Fraction of a push covered by the sponsor.
- * @returns Round-up € funded by the sponsor, limited to the cap.
- */
-function discountRoundupValue(
-	capRemainingBeforeAmount: number,
-	amount: number,
-	pushprice: number,
-	discountFraction: number
-) {
-	const pushprice_discounted = (pushprice * (1 - discountFraction));
-	const roundup_value = (pushprice - pushprice_discounted);
-	return Math.min(
-		((amount / pushprice_discounted) * roundup_value),
-		(capRemainingBeforeAmount - amount)
-	);
-}
-
-function isOneTime() {
-	return onetime.checked;
-}
-
-function cycle() {
-	return isOneTime() ? "onetime" : "monthly";
-}
-
-function validateForm(data, actions) {
-	for (const el of form.querySelectorAll("input[required]")) {
-		if (!(el as HTMLInputElement).reportValidity()) {
-			actions.reject();
-			return false;
-		}
-	}
-	return true;
-}
-
-function startTransaction() {
-	error.hidden = true;
-	document.querySelector("html")!.classList.add("wait");
-}
-
-function endTransaction() {
-	document.querySelector("html")!.classList.remove("wait");
-}
-
-function thankyou() {
-	return form.submit();
-}
-
-async function sendIncoming(orderID: string) {
-	let response = await fetch('/api/transaction-incoming', {
-		method: 'post',
-		headers: {
-			'content-type': 'application/json'
-		},
-		body: JSON.stringify({
-			ProviderSession: orderID,
-			CustName: cust_name.value,
-			CustURL: cust_url.value,
-			Metric: metric.value,
-			Goal: goal.value,
-			Micro: micro.checked,
-			Cycle: cycle(),
-			Discount: ((isOneTime() && discount) ? discount.selectedIndex : 0),
-			Cents: (Number(amount.value)) * 100,
-		})
-	});
-	if(!response.ok) {
-		error.innerHTML =
-			"Something went wrong: " + await response.text() + "<br>" +
-			"I should have received your order though, and will confirm it " +
-			"as soon as I see it.";
-		error.hidden = false;
-		endTransaction();
-		return false;
-	}
-	return true;
+function paypalValidate(data, actions) {
+	const ret = validateForm();
+	!ret && actions.reject();
+	return ret;
 }
 
 async function paypalSubmit(data, actions) {
 	if(await sendIncoming(data.orderID)) {
-		 thankyou();
+		thankyou();
 	}
 }
 
@@ -127,7 +28,7 @@ let subscription = {
 	},
 	onApprove: paypalSubmit,
 	onCancel: endTransaction,
-	onClick: validateForm,
+	onClick: paypalValidate,
 };
 
 let order = {
@@ -142,90 +43,14 @@ let order = {
 		await paypalSubmit(data, actions);
 	},
 	onCancel: endTransaction,
-	onClick: validateForm,
+	onClick: paypalValidate,
 };
 
-function clampNumber(obj) {
-	obj.value = Math.max(obj.value, obj.min);
-	obj.value = Math.min(obj.value, obj.max);
-	return obj
-}
-
-function formatNumber(obj, digits) {
-	return parseFloat(obj.value).toFixed(digits);
-}
-
-function onCycle() {
+function paypalOnCycle(onetime: boolean) {
 	let button_id = 'paypal-button-container';
 	let button_selector = '#' + button_id;
 	let button_container = document.getElementById(button_id)!;
 
-	let push_amount = document.getElementById("push_amount")!;
-	let push_noun = document.getElementById("push_noun")!;
-
-	const pushprice = (Number(push_amount.dataset.price) / 100);
-
-	const updatePushAmount = function(
-		target_amount: HTMLElement, target_noun: HTMLElement, money: number
-	) {
-		target_amount.innerHTML = (
-			(Math.round((money / pushprice) * 100) / 100).toString()
-		);
-		target_noun.innerHTML = ((money == pushprice) ? " push" : " pushes");
-	}
-
 	button_container.innerHTML = "";
-	if(isOneTime()) {
-		paypal.Buttons(order).render(button_selector);
-		amount.onchange = function() {
-			const value_from_customer = clampNumber(amount);
-			updatePushAmount(push_amount, push_noun, Number(amount.value));
-			amount.value = formatNumber(value_from_customer, 2);
-
-			if(!discount) {
-				return;
-			}
-
-			const discount_offer = discount.options[discount.selectedIndex];
-			const discount_fraction = Number(discount_offer.dataset.fraction);
-			if(discount_offer.index === 0) {
-				discount_breakdown.hidden = true;
-			} else {
-				discount_breakdown.hidden = false;
-				discount_sponsor.innerHTML = discount_offer.dataset.name!;
-				const roundup_value = discountRoundupValue(
-					Number(amount.max),
-					Number(amount.value),
-					pushprice,
-					discount_fraction
-				)
-				updatePushAmount(roundup_pushes, roundup_noun, roundup_value);
-				roundup_amount.innerHTML = valueInCurrency(roundup_value * 100);
-			}
-		}
-		amount.min = "1.00";
-		amount.step = "0.01";
-
-		if(discount) {
-			discount.disabled = false;
-			discount_breakdown.hidden = false;
-		}
-	} else {
-		paypal.Buttons(subscription).render(button_selector);
-		amount.onchange = function() {
-			amount.value = formatNumber(amount, 0);
-			updatePushAmount(push_amount, push_noun, Number(amount.value));
-		}
-		amount.min = "1";
-		amount.step = "1";
-
-		if(discount) {
-			discount.disabled = true;
-			discount.selectedIndex = 0;
-			discount_breakdown.hidden = true;
-		}
-	}
-	amount.onchange();
+	paypal.Buttons(onetime ? order : subscription).render(button_selector);
 }
-
-onCycle();
